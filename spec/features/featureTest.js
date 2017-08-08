7a68aa10e892ac82b8fb8eafe00af5b50acfa851
Ryan Davidson <daviryan@aol.com>
2017-08-08 11:45:43 +0100

    Start feature test with mocha and zombie

---
+++ spec/features/featureTest.js
@@ -0,0 +1,9 @@
+process.env.NODE_ENV = 'test';
+var Browser = require('zombie');
+// http://www.redotheweb.com/2013/01/15/functional-testing-for-nodejs-using-mocha-and-zombie-js.html
+
+describe('User visits home page', function() {
+
+  it('test');
+
+});
--- src/interface.js
+++ src/interface.js
@@ -3,23 +3,25 @@ $(document).ready(function(){
 
   function displayAdverts() {
     var string = "";
+    var index = 0;
     list.adverts().forEach(function(advert){
-      string += "<li>" + advert.name() + " - Booked: " + advert.isBooked() + "</li>";
+      string += "<li class='.advert1'>" + advert.name() + " - Booked: " + advert.isBooked() + "</li>";
+      index += 1;
     });
-    $(".advert-list").html(string);
+    $(".advertList").html(string);
   }
 
-  $(".add-advert").click(function() {
+  $(".addAdvert").click(function() {
     if (list.adverts().length >= 1) {
       return;
     }
-    var name = $(".advert-name").val();
+    var name = $(".advertName").val();
     var advert = new Advert(name);
     list.add(advert);
     displayAdverts();
   });
 
-  $(".book").click(function() {
+  $(".bookAdvert").click(function() {
     list.adverts()[0].book();
     displayAdverts();
   });
