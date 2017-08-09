0161fff3acbb2ca5e078fd3b630bd9f182973136
antoinedoolaeghe <antoinedoolaeghe@gmail.com>
2017-08-09 16:17:54 +0100

    add button for new advert form and pass feature tests

--- test/features/confirmBooking.js
+++ test/features/confirmBooking.js
@@ -10,23 +10,17 @@ describe('Confirm a booking', function() {
 
   before(function() {
     this.server = http.createServer(app).listen(3000);
-    // initialize the browser using the same port as the test application
     this.browser = new Browser({ site: 'http://localhost:3000' });
   });
 
-  // load the contact page
+
   before(function(done) {
     this.browser.visit('/', done);
   });
 
-  it('should display true when bookAdvert button is clicked', function(){
+  it('should display a signup button', function(){
     var browser = this.browser;
-    browser.fill('advertName', 'home');
-    browser.pressButton('addAdvert').then(function() {
-      browser.pressButton('bookAdvert').then(function() {
-        assert.equal(browser.text('.advert1Booked'), 'true');
-      });
-    });
+    browser.assert.element('.signUp');
   });
 
   after(function(done) {
