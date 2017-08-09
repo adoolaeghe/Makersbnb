4724ddaf369c712c727378a02588a34c5a7d9872
Ryan Davidson <daviryan@aol.com>
2017-08-09 09:42:20 +0100

    partials ejs

--- app.js
+++ app.js
@@ -3,26 +3,35 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var http = require('http');
 var fs   = require('fs');
+var EJS  = require('ejs');
+
+// app.engine('html', EJS.renderFile);
 
 var app = express();
 
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/app/views/');
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname, 'app')));
+//app.use(express.static(path.join(__dirname, 'app')));
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname+'/app/views/index.html'));
+  //res.sendFile(path.join(__dirname+'/app/views/index.html'));
+  res.render('index');
 });
 
 app.get('/users/new', function(req, res) {
-  res.sendFile(path.join(__dirname+'/app/views/users/new.html'));
+  res.render('users/new');
+//  res.sendFile(path.join(__dirname+'/app/views/users/new.html'));
 });
 
 app.post('/users/new', function(req, res) {
   var name = req.body.name;
+  var username = req.body.username;
+  var email = req.body.username;
+  var password = req.body.password;
+  var passwordConfirmation = req.body.passwordConfirmation;
   console.log(name);
   res.send(name);
   //res.sendFile(path.join(__dirname+'/app/views/users/new.html'));
